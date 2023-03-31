e888617dbc0b25acae3db5b0d5cb3435d05066d0
klxiaoniu <klxiaoniu@proton.me>
2023-03-31 20:24:00 +0800

    confirm dialog

--- app/src/main/java/com/glittering/youxi/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/glittering/youxi/ui/fragment/home/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.glittering.youxi.R
@@ -13,6 +15,7 @@ import com.glittering.youxi.databinding.FragmentHomeBinding
 import com.glittering.youxi.ui.activity.IntroActivity
 import com.glittering.youxi.ui.activity.LoginActivity
 import com.glittering.youxi.ui.activity.ProfileUpdateActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.hjq.toast.Toaster
 
 class HomeFragment : Fragment() {
@@ -22,11 +25,13 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
     companion object {
         val instance: HomeFragment by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
             HomeFragment()
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -64,7 +69,31 @@ class HomeFragment : Fragment() {
             Toaster.setView(R.layout.toast_fail)
             Toaster.show("账号或密码不正确。账号或密码不正确。账号或密码不正确。账号或密码不正确。")
         }
-
+        binding.btnConfirmDialog.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.dialog_view, null)
+            view.findViewById<TextView>(R.id.message)
+                .text = "确定操作？"
+            val dialog = MaterialAlertDialogBuilder(requireActivity())
+                .setTitle("提示")
+                .setView(view)
+                .setPositiveButton("确定") { dialog, which ->
+                    // Respond to positive button press
+                }
+                .setNegativeButton("取消") { dialog, which ->
+                    // Respond to negative button press
+                }
+                .show()
+            val btnPositive = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            val btnNegative = dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+            btnPositive.setBackgroundColor(resources.getColor(R.color.primary_yellow, null))
+            btnPositive.setTextColor(resources.getColor(R.color.black, null))
+            btnNegative.setBackgroundColor(resources.getColor(R.color.primary_yellow_2, null))
+            btnNegative.setTextColor(resources.getColor(R.color.black, null))
+            btnPositive.setPadding(80, 50, 80, 50)
+            btnNegative.setPadding(80, 50, 80, 50)
+            btnPositive.setTextSize(18f)
+            btnNegative.setTextSize(18f)
+        }
         return root
     }
 
