ff621bdd7db88d903553d4e4f468ab4265c3af53
klxiaoniu <klxiaoniu@proton.me>
2023-05-05 22:15:40 +0800

    use icons
    Signed-off-by: klxiaoniu <klxiaoniu@proton.me>

--- app/src/main/java/com/glittering/youxi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/glittering/youxi/ui/activity/MainActivity.kt
@@ -2,7 +2,6 @@ package com.glittering.youxi.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.glittering.youxi.R
@@ -12,6 +11,8 @@ import com.glittering.youxi.ui.fragment.dashboard.DashboardFragment
 import com.glittering.youxi.ui.fragment.home.HomeFragment
 import com.glittering.youxi.ui.fragment.me.MeFragment
 import com.glittering.youxi.ui.fragment.notifications.NotificationsFragment
+import com.glittering.youxi.utils.BottomNavigationViewUtil
+import com.glittering.youxi.utils.TabItem
 import com.glittering.youxi.utils.ToastSuccess
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -23,15 +24,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         val navView: BottomNavigationView = binding.navView
         val mainViewPager: ViewPager2 = binding.mainViewPager
 
-        mainViewPager.isUserInputEnabled = false
-        mainViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                //设置导航栏选中位置
-                navView.menu.getItem(if (position > 1) position + 1 else position).isChecked = true
-//                findViewById<View>(R.id.action_search)?.visibility =
-//                    if (position == 0) View.VISIBLE else View.GONE
-            }
-        })
+//        mainViewPager.isUserInputEnabled = false
+//        mainViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+//            override fun onPageSelected(position: Int) {
+//                //设置导航栏选中位置
+//                navView.menu.getItem(if (position > 1) position + 1 else position).isChecked = true
+////                findViewById<View>(R.id.action_search)?.visibility =
+////                    if (position == 0) View.VISIBLE else View.GONE
+//            }
+//        })
 
         val fragmentArr = ArrayList<Fragment>()
         fragmentArr.add(HomeFragment.instance)
@@ -40,34 +41,61 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         fragmentArr.add(MeFragment.instance)
         mainViewPager.adapter = PagerAdapter(this, fragmentArr)
 
-        navView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.navigation_home -> {
-                    mainViewPager.setCurrentItem(0, false)
-                }
-                R.id.navigation_dashboard -> {
-                    mainViewPager.setCurrentItem(1, false)
-                }
-                R.id.navigation_notifications -> {
-                    mainViewPager.setCurrentItem(2, false)
-                }
-                R.id.navigation_me -> {
-                    mainViewPager.setCurrentItem(3, false)
-                    MeFragment.instance.updateUserInfo()
-                }
-                R.id.navigation_sell -> {
-                    //TODO: Launch sell activity
-                    ToastSuccess("Debug")
-                    val intent = Intent(this, DebugActivity::class.java)
-                    startActivity(intent)
-                }
-            }
-            Log.d(
-                "onItemSelected",
-                it.itemId.toString() + " " + mainViewPager.currentItem.toString()
-            )
+        val tabItems = ArrayList<TabItem>()
+        tabItems.add(TabItem("首页", R.drawable.ic_tab_home))
+        tabItems.add(TabItem("我要买", R.drawable.ic_tab_wantbuy))
+        tabItems.add(TabItem("", R.drawable.ic_wantsell, true))
+        tabItems.add(TabItem("消息", R.drawable.ic_tab_notification))
+        tabItems.add(TabItem("我的", R.drawable.ic_tab_me))
 
-            true
-        }
+        val util = BottomNavigationViewUtil.getInstance()
+            .init(
+                this,
+                binding.navView,
+                binding.mainViewPager,
+                fragmentArr,
+                tabItems
+            ).addOnlyOperateClick {
+                //TODO: Launch sell activity
+                ToastSuccess("Debug")
+                val intent = Intent(this, DebugActivity::class.java)
+                startActivity(intent)
+            }
+
+//        binding.navView.postDelayed({
+//            util.addTabItemRedCirclePoint(3, 58)
+//            util.addTabItemRedCirclePoint(0, 21)
+//
+//        }, 5 * 1000)
+
+//        navView.setOnItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.navigation_home -> {
+//                    mainViewPager.setCurrentItem(0, false)
+//                }
+//                R.id.navigation_dashboard -> {
+//                    mainViewPager.setCurrentItem(1, false)
+//                }
+//                R.id.navigation_notifications -> {
+//                    mainViewPager.setCurrentItem(2, false)
+//                }
+//                R.id.navigation_me -> {
+//                    mainViewPager.setCurrentItem(3, false)
+//                    MeFragment.instance.updateUserInfo()
+//                }
+//                R.id.navigation_sell -> {
+//                    //TODO: Launch sell activity
+//                    ToastSuccess("Debug")
+//                    val intent = Intent(this, DebugActivity::class.java)
+//                    startActivity(intent)
+//                }
+//            }
+//            Log.d(
+//                "onItemSelected",
+//                it.itemId.toString() + " " + mainViewPager.currentItem.toString()
+//            )
+//
+//            true
+//        }
     }
 }
