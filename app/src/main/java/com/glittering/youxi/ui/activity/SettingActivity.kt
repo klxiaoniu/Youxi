659fa91ed7317e8879fd8efae43e1ae752ba7e4f
klxiaoniu <klxiaoniu@proton.me>
2023-05-29 22:05:51 +0800

    feat: password, about, logout
    Signed-off-by: klxiaoniu <klxiaoniu@proton.me>

--- app/src/main/java/com/glittering/youxi/ui/activity/SettingActivity.kt
+++ app/src/main/java/com/glittering/youxi/ui/activity/SettingActivity.kt
@@ -1,16 +1,30 @@
 package com.glittering.youxi.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.glittering.youxi.MyApplication.Companion.loggedInUser
 import com.glittering.youxi.R
 import com.glittering.youxi.databinding.ActivitySettingBinding
+import com.glittering.youxi.utils.DialogUtil
+import com.glittering.youxi.utils.rmToken
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.gyf.immersionbar.ktx.fitsTitleBar
 
 class SettingActivity : BaseActivity<ActivitySettingBinding>() {
 
+    override val fitSystemWindows = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.toolbar.let {
+            it.setNavigationIcon(R.drawable.ic_back)
+            it.setNavigationOnClickListener { finish() }
+            fitsTitleBar(it)
+        }
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
@@ -23,7 +37,36 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
             findPreference<Preference>("skin")?.onPreferenceChangeListener = this
+            findPreference<Preference>("password")?.setOnPreferenceClickListener {
+                val intent = Intent(requireContext(), ProfileUpdateActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            findPreference<Preference>("about")?.setOnPreferenceClickListener {
+                val dialog = MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("关于游兮")
+                    .setMessage("游兮致力于为广大游戏玩家提供一个安全、便捷的游戏交易平台。\n\nMade by Glittering with love")
+                    .setPositiveButton("确定", null)
+                    .show()
+                DialogUtil.stylize(dialog)
+                true
+            }
+            findPreference<Preference>("logout")?.setOnPreferenceClickListener {
+                val dialog = MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("退出登录")
+                    .setMessage("确定要退出登录吗？")
+                    .setPositiveButton("确定") { _, _ ->
+                        rmToken()
+                        loggedInUser = null
+                        requireActivity().finish()
+                    }
+                    .setNegativeButton("取消", null)
+                    .show()
+                DialogUtil.stylize(dialog)
+                true
+            }
         }
+
 
         override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
             AppCompatDelegate.setDefaultNightMode(
