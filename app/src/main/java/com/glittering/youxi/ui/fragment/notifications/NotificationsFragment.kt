756ff9ee6a697633fe23188b0d542f98980c76d0
klxiaoniu <klxiaoniu@proton.me>
2023-06-01 20:19:41 +0800

    feat: permission check and refactor with viewmodel
    Signed-off-by: klxiaoniu <klxiaoniu@proton.me>

--- app/src/main/java/com/glittering/youxi/ui/fragment/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/glittering/youxi/ui/fragment/notifications/NotificationsFragment.kt
@@ -1,18 +1,21 @@
 package com.glittering.youxi.ui.fragment.notifications
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.glittering.youxi.R
-import com.glittering.youxi.data.Notification
 import com.glittering.youxi.data.NotificationAdapter
 import com.glittering.youxi.databinding.FragmentNotificationsBinding
+import com.glittering.youxi.utils.DialogUtil
 import com.glittering.youxi.utils.applicationContext
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class NotificationsFragment : Fragment() {
 
@@ -40,25 +43,50 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val list: List<Notification> = listOf(
-            Notification(10000, "系统通知", "系统通知内容", 0, R.drawable.ic_notifications_black_24dp),
-            Notification(2, "聊天消息", "聊天消息内容", 0, R.drawable.ic_success)
-        )
-        adapter = NotificationAdapter(list,requireActivity())
+        adapter = NotificationAdapter(emptyList(), requireActivity())
         val layoutManager = LinearLayoutManager(applicationContext)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
         binding.recyclerview.layoutManager = layoutManager
         binding.recyclerview.adapter = adapter
-
+        notificationsViewModel.notificationList.observe(requireActivity()) {
+            adapter.setAdapterList(it)
+        }
 
         binding.editTextSearch.addTextChangedListener {
             adapter.filter(it.toString().trim())
         }
+
+        checkPermission()
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && PermissionChecker.checkSelfPermission(
+                requireContext(),
+                "android.permission.POST_NOTIFICATIONS"
+            ) != PermissionChecker.PERMISSION_GRANTED
+        ) {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("权限申请")
+                .setMessage("为向您推送聊天消息、系统通知等，游兮需要您授予通知权限。请您在弹出的对话框中点击“始终允许”。")
+                .setPositiveButton("确定") { _, _ ->
+                    ActivityCompat.requestPermissions(
+                        requireActivity(),
+                        arrayOf("android.permission.POST_NOTIFICATIONS"),
+                        1
+                    )
+                }
+                .setNegativeButton("取消", null)
+                .setCancelable(false)
+                .show()
+                .let {
+                    DialogUtil.stylize(it)
+                }
+        }
     }
 }
--- app/src/main/java/com/glittering/youxi/ui/fragment/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/glittering/youxi/ui/fragment/notifications/NotificationsViewModel.kt
@@ -1,13 +1,18 @@
 package com.glittering.youxi.ui.fragment.notifications
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.glittering.youxi.R
+import com.glittering.youxi.data.Notification
 
 class NotificationsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    val notificationList = MutableLiveData<List<Notification>>()
+
+    init {
+        notificationList.value = listOf(
+            Notification(10000, "系统通知", "系统通知内容", 0, R.drawable.ic_notifications_black_24dp),
+            Notification(2, "聊天消息", "聊天消息内容", 0, R.drawable.ic_success)
+        )
     }
-    val text: LiveData<String> = _text
 }
