948b4c23a05211fdaa7e564dcbe2ebd98f760333
klxiaoniu <klxiaoniu@proton.me>
2023-06-20 23:17:55 +0800

    fix: msg_database switch when userId changes
    Signed-off-by: klxiaoniu <klxiaoniu@proton.me>

--- app/src/main/java/com/glittering/youxi/database/MsgDatabase.kt
+++ app/src/main/java/com/glittering/youxi/database/MsgDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.glittering.youxi.dao.MsgRecordDao
 import com.glittering.youxi.entity.MsgRecord
+import com.glittering.youxi.utils.UserStateUtil
 import com.glittering.youxi.utils.applicationContext
 
 @Database(entities = [MsgRecord::class], version = 1)
@@ -13,13 +14,16 @@ abstract class MsgDatabase : RoomDatabase() {
 
     companion object {
         private var instance: MsgDatabase? = null
+        private var userId: Long? = null
 
         @Synchronized
         fun getDatabase(): MsgDatabase {
-            if (instance == null) {
+            if (instance == null || userId != UserStateUtil.getInstance().getUserId()) {
+                userId = UserStateUtil.getInstance().getUserId()
                 instance = Room.databaseBuilder(
                     applicationContext,
-                    MsgDatabase::class.java, "msg_database"
+                    MsgDatabase::class.java,
+                    "msg_database_$userId"
                 )
                     .build()
             }
